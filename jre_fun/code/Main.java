578e4c0872ed7b4aebcf70e30be932cbc15c658b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-11-21 13:37:10 +0200

    first

---
+++ jre_fun/code/Main.java
@@ -0,0 +1,16 @@
+/**
+* Tru la la
+*/
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String [] args) {
+		String s = "";
+		Scanner sc = new Scanner(System.in);
+
+		while (true){
+			s = sc.next();
+			System.out.println(s);
+		}
+	}
+}
