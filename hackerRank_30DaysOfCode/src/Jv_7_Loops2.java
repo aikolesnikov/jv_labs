87c40fa4e295c1f9a9a84fbefe5e114d80a8a849
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-11-21 19:44:35 +0200

    6

---
+++ hackerRank_30DaysOfCode/src/Jv_7_Loops2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Jv_7_Loops2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int s = a;
+            int k2 = 1;
+
+            for (int j = 0; j < n; j++) {
+                s = s + k2 * b;
+                System.out.print(s + " ");
+                k2 = k2 * 2;
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
