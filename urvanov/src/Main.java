bcaea9c288325f27e30b68b4a7abf3a6dd73470b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-08 16:45:51 +0200

    urvanov_2

--- urvanov/src/Main.java
+++ urvanov/src/Main.java
@@ -1,10 +1,77 @@
+import static java.lang.Float.MAX_VALUE;
+import static java.lang.Float.NaN;
+import static java.lang.Float.POSITIVE_INFINITY;
+
 public class Main {
+
+    //---------------
+    //---  Literals
+    //---------------
+    static int[] iArr = {1, 02, 0x3, 0b100, 2_147_483_647, 0177_7777_7777, 0x7fff_ffff,
+            0b0111_1111_1111_1111_1111_1111_1111_1111};
+    //        for (int i:iArr) System.out.println(i);
+
+    static long[] lArr = {1L, 02L, 0x3L, 0b100L, 2_147_483_647L, 0177_7777_7777L, 0x7fff_ffffL,
+            0b0111_1111_1111_1111_1111_1111_1111_1111L};
+    //        for (long l:lArr) System.out.println(l);
+
+    static float[] fArr = {3.14f, .14f, POSITIVE_INFINITY, MAX_VALUE, NaN};
+    //        for (float f:fArr) System.out.println(f);
+
+    static double[] dArr = {3.14, 3.14d, .14d, Double.MAX_VALUE};
+    //        for (double d:dArr) System.out.println(d);
+
+    static boolean[] bArr = {true, false};
+    //        for (boolean b:bArr) System.out.println(b);
+
+    static char[] cArr = {'a', '\r', 'W', '\uFFFF', '%', '\'', '\177', '\t', '\u03a9'};
+    //        for (char c:cArr) System.out.println(c);
+
+
+    //---------------
+    //---  Variables
+    //---------------
+    static int varStatic1;  //  Class Variables (Static Fields)
+    int varnNonStatic1;     //  Instance Variables (Non-Static Fields)
+
+    void method1(int n) {   //  Parameters
+        int m;              //  Local Variables
+    }
+
+
+    //---------------
+    //---  Data types
+    //---------------
+    static byte byteVar1;             // 1 byte 2^8       -128 +127
+    static short shortVar1;            // 2 bytes 2^16     -32'768 +32'767
+    static int integerVar1;              // 4 bytes 2^32     -2^31 +2^31
+    static long longVar1;             // 8 bytes 2^64     -2^64 +2^64
+    static float floatVar1;            // 32 bit
+    static double doubleVal1;           // 64 bit
+    static boolean booleanVal1;          // true, false
+    static char charVal1;             // 2 bytes
 
 
     public static void main(String[] args) {
-        System.out.println("Urvanov is started.");
 
         // Variables
+        varStatic1 = 1;
+
+        Main o = new Main();
+        o.varnNonStatic1 = 2;
+
+        System.out.println(o.varnNonStatic1);
+        System.out.println(varStatic1);
+
+        // Data types
+        System.out.println(Byte.TYPE + " default " + byteVar1 + " " + "min " + Byte.MIN_VALUE + " max " + Byte.MAX_VALUE);
+        System.out.println(Short.TYPE + " default " + shortVar1 + " " + "min " + Short.MIN_VALUE + " max " + Short.MAX_VALUE);
+        System.out.println(Integer.TYPE + " default " + integerVar1 + " " + "min " + Integer.MIN_VALUE + " max " + Integer.MAX_VALUE);
+        System.out.println(Long.TYPE + " default " + longVar1 + " " + "min " + Long.MIN_VALUE + " max " + Long.MAX_VALUE);
+        System.out.println(Float.TYPE + " default " + floatVar1 + " " + "min " + Float.MIN_VALUE + " max " + Float.MAX_VALUE);
+        System.out.println(Double.TYPE + " default " + doubleVal1 + " " + "min " + Double.MIN_VALUE + " max " + Double.MAX_VALUE);
+        System.out.println(Boolean.TYPE + " default " + booleanVal1 + " " + "min " + Boolean.FALSE + " max " + Boolean.TRUE);
+        System.out.println(Character.TYPE + " default " + charVal1 + " " + "min " + Character.MIN_VALUE + " max " + Character.MAX_VALUE);
 
     }
 }
--- urvanov/src/Variables.java
+++ urvanov/src/Variables.java
@@ -1,12 +1,4 @@
 class Variables {
 
-    static int varStatic1;  //  Class Variables (Static Fields)
-    int nonStatic1;         //  Instance Variables (Non-Static Fields)
-
-    void method1(int n) {   //  Parameters
-
-        int m;              //  Local Variables
-
-    }
 
 }
