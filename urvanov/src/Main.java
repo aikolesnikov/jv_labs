72b56dea59e793e7cf7cf962d00df48ae8e40a5b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-08 17:56:59 +0200

    urvanov_2

--- urvanov/src/Main.java
+++ urvanov/src/Main.java
@@ -9,30 +9,25 @@ public class Main {
     //---------------
     static int[] iArr = {1, 02, 0x3, 0b100, 2_147_483_647, 0177_7777_7777, 0x7fff_ffff,
             0b0111_1111_1111_1111_1111_1111_1111_1111};
-    //        for (int i:iArr) System.out.println(i);
 
     static long[] lArr = {1L, 02L, 0x3L, 0b100L, 2_147_483_647L, 0177_7777_7777L, 0x7fff_ffffL,
             0b0111_1111_1111_1111_1111_1111_1111_1111L};
-    //        for (long l:lArr) System.out.println(l);
 
     static float[] fArr = {3.14f, .14f, POSITIVE_INFINITY, MAX_VALUE, NaN};
-    //        for (float f:fArr) System.out.println(f);
 
     static double[] dArr = {3.14, 3.14d, .14d, Double.MAX_VALUE};
-    //        for (double d:dArr) System.out.println(d);
 
     static boolean[] bArr = {true, false};
-    //        for (boolean b:bArr) System.out.println(b);
 
     static char[] cArr = {'a', '\r', 'W', '\uFFFF', '%', '\'', '\177', '\t', '\u03a9'};
-    //        for (char c:cArr) System.out.println(c);
 
+    static Object[] objArr = {null, "abc", new Main(), String.class};
 
     //---------------
     //---  Variables
     //---------------
     static int varStatic1;  //  Class Variables (Static Fields)
-    int varnNonStatic1;     //  Instance Variables (Non-Static Fields)
+    public int varNonStatic1;     //  Instance Variables (Non-Static Fields)
 
     void method1(int n) {   //  Parameters
         int m;              //  Local Variables
@@ -54,14 +49,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Literals
+        for (int i : iArr) System.out.println(i);
+        for (long l : lArr) System.out.println(l);
+        for (float f : fArr) System.out.println(f);
+        for (double d : dArr) System.out.println(d);
+        for (boolean b : bArr) System.out.println(b);
+        for (char c : cArr) System.out.println(c);
+        for (Object o : objArr) System.out.println(o);
+        System.out.println();
+
+
         // Variables
         varStatic1 = 1;
+        System.out.println("NewObj nonStatic " + new Main().varNonStatic1);
+        System.out.println();
 
-        Main o = new Main();
-        o.varnNonStatic1 = 2;
-
-        System.out.println(o.varnNonStatic1);
-        System.out.println(varStatic1);
 
         // Data types
         System.out.println(Byte.TYPE + " default " + byteVar1 + " " + "min " + Byte.MIN_VALUE + " max " + Byte.MAX_VALUE);
