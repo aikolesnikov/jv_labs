595c1a0cfa783fa12d0381eab38c1b9e9b1aa675
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-12 16:22:19 +0200

    hack_r_13_Formatting

---
+++ hackerRank_30DaysOfCode/src/Jv_13_CurrencyFormatter.java
@@ -0,0 +1,23 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Jv_13_CurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat india = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+
+}
