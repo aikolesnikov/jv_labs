b88215148db1093e3e2b8b30a46d46d902420394
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-14 10:53:19 +0200

    hack_r_2_Strings_1

---
+++ hackerRank_30DaysOfCode/src/Jv_2_String_03.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Jv_2_String_03 {
+
+    private static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        if (s.length() >= k) {
+            smallest = largest = s.substring(0, k);
+        }
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            String st = s.substring(i, i + k);
+            if (st.compareTo(smallest) < 0) {
+                smallest = st;
+            }
+            if (st.compareTo(largest) > 0) {
+                largest = st;
+            }
+
+            // System.out.println(st + " " + smallest + " " + largest);
+        }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
---
+++ hackerRank_30DaysOfCode/src/Jv_72_Reflection.java
@@ -0,0 +1,79 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student2 {
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void anothermethod() {
+    }
+
+    public void anothermethod2() {
+    }
+
+}
+
+public class Jv_72_Reflection {
+
+    public static void main(String[] args){
+
+        Class student = Student2.class;
+
+       /* Get declared methods */
+        Method[] methods = student.getDeclaredMethods();
+
+       /* Adding method list to a collection */
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method m : methods){
+            methodList.add(m.getName());
+        }
+
+       /* Sorting the list of methods */
+        Collections.sort(methodList);
+
+       /* Printing the methods list */
+        for(String name: methodList){
+            System.out.println(name);
+        }
+
+/*
+       // The following blocks is EXTRA exercise.
+
+       // Getting and Printing the list of constructors Student class
+       Constructor<Student>[] constructsOfStudent = student.getConstructors();
+       if (constructsOfStudent.length != 0){
+           System.out.println("Constructs of Student");
+           for (Constructor c : constructsOfStudent){
+               System.out.println(c.toGenericString());
+           }
+       }
+       else System.out.println("No constructors in " + student.getSimpleName() + "class");
+
+       // Getting and Printing the Fields of Student class
+       Field[] fields = student.getFields();
+       if (fields.length != 0){
+          System.out.println("List of fields in Student class");
+             for (Field f : fields){
+                 System.out.println(f.toGenericString());
+             }
+       }
+       else System.out.println("No fields in " + student.getSimpleName() + "class");
+*/
+    }
+}
