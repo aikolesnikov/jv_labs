7532b8277ae5bcf69942e7689c4d7a34c33212bc
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-25 23:39:29 +0200

    test from notebook

--- hackerRank_30DaysOfCode/src/Jv_12_DateAndTime.java
+++ hackerRank_30DaysOfCode/src/Jv_12_DateAndTime.java
@@ -9,14 +9,14 @@ public class Jv_12_DateAndTime {
     private static String getDay(String day, String month, String year) {
 
         // by LocalDate
-        LocalDate dt = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
-        return dt.getDayOfWeek().name();
+        // LocalDate dt = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        // return dt.getDayOfWeek().name();
 
         // by Calendar
-        // Calendar cDate = Calendar.getInstance();
-        // cDate.set(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        Calendar cDate = Calendar.getInstance();
+        cDate.set(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
 
-        // return cDate.getDisplayName(Calendar.DAY_OF_WEEK, 2, Locale.getDefault()).toUpperCase();
+        return cDate.getDisplayName(Calendar.DAY_OF_WEEK, 2, Locale.getDefault()).toUpperCase();
 
     }
 
@@ -28,7 +28,7 @@ public class Jv_12_DateAndTime {
         // String year = in.next();
 
         // System.out.println(getDay(day, month, year));
-        System.out.println(getDay("5", "8", "2015"));
+        System.out.println(getDay("5", "7", "2015"));
 
     }
 
