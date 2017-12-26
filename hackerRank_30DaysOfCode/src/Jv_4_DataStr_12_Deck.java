aa919e33756be35a7ee2084755164b9b939fb9dd
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-26 16:44:12 +0200

    hack_r_2_DataStr_12_

--- hackerRank_30DaysOfCode/src/Jv_4_DataStr_12_Deck.java
+++ hackerRank_30DaysOfCode/src/Jv_4_DataStr_12_Deck.java
@@ -11,28 +11,45 @@ public class Jv_4_DataStr_12_Deck {
         int n = in.nextInt();
         int m = in.nextInt();
 
+        int tSum = 0;
+        HashSet<Integer> hs = new HashSet<>();
+
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
+
             deque.add(num);
-        }
+            hs.add(num);
 
+            if (deque.size() == m + 1) {
 
-        Object[] ar = deque.toArray();
-        int tSum = 0;
-        for (int i = 0; i <= n - m; i++) {
-            HashSet<Integer> hs = new HashSet<>();
-            for (int j = 0; j < m; j++) {
-                hs.add(((Integer) ar[i + j]));
+                int tElem = (int) deque.remove();
+                if (!deque.contains(tElem)) {
+                    hs.remove(tElem);
+                }
             }
+
             if (hs.size() > tSum)
                 tSum = hs.size();
-
-//            for (int j = 0; j < m; j++) {
-//                System.out.println(((Integer) ar[i + j]) + " ");
-//            }
-//            System.out.println();
-
         }
+
+
+//  slow solution
+//        Object[] ar = deque.toArray();
+//        int tSum = 0;
+//        for (int i = 0; i <= n - m; i++) {
+//            HashSet<Integer> hs = new HashSet<>();
+//            for (int j = 0; j < m; j++) {
+//                hs.add(((Integer) ar[i + j]));
+//            }
+//            if (hs.size() > tSum)
+//                tSum = hs.size();
+//
+////            for (int j = 0; j < m; j++) {
+////                System.out.println(((Integer) ar[i + j]) + " ");
+////            }
+////            System.out.println();
+//
+//        }
 
         System.out.println(tSum);
 
