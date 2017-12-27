6b4338dc21b6b6e6ac406d537d7343e53755c09e
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-12-27 13:41:38 +0200

    hack_r_jv_Algo_1_1

---
+++ hackerRank_30DaysOfCode/src/Jv_Algo_1_Warmup_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Jv_Algo_1_Warmup_1 {
+
+    static int solveMeFirst(int a, int b) {
+        // Hint: Type return a+b; below
+
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
+
