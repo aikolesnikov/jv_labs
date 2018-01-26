46842f8c7f03da849750a365b29ff0ace8c86a35
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-01-26 19:20:23 +0200

    vertex init

---
+++ vertex/src/main/java/entry_test/EntryTest.java
@@ -0,0 +1,58 @@
+package entry_test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntryTest {
+
+    // 1. Написать метод заполнения списка случайными целыми числами от 0 до 100. Метод принимает список и количество элементов. 
+    public void fillListWithRandom(List<Number> numberList, int numQty) {
+
+    }
+
+    // 2. Написать метод, который принимает имя файла и возвращает количество строк в файле (задача на применение FileReader).
+    public long getFileRowsCount(String fileName) {
+
+        return 0;
+    }
+
+    /*
+???
+3. Написать метод, принимающий двумерный квадратный массив и заполняющий:
+01, 02, 03, 04
+05, 06, 07, 08
+09, 10, 11, 12
+13, 14, 15, 16
+ */
+
+    public int[][] fillArray() {
+
+        int[][] arInt = new int[][]{};
+        return arInt;
+    }
+
+    // 4. Написать метод, проверяющий является ли строка полиндромом. Строка, которая одинаково читается справо-налево и слева-направо. Например, фииф, нгшшгн
+
+    public static boolean isPolindrom(String s) {
+
+        return false;
+    }
+
+
+    /*
+        5. Написать метод, который принимает массив целых чисел, заполняет по такому принципу:
+        - элементы с четными индексами - значением номера элемента
+        - элементы с нечетными индексами - значением 0
+        Вывести массив на экран 1, 0, 3, 0, 5, 0, 7, 0..
+     */
+    public void vv(){
+
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hi, Vertex!");
+    }
+
+}
